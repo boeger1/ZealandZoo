8215d95e11981a89fc82a2ac9c7d4db5eb85f6dc
boeger01 <matias.boeger@gmail.com>
2023-04-19 12:15:53 +0200

    skrev oplysninger forkert sted alt rigtigt nu

--- ZealandZooLIB/Secrets/ISecret.cs
+++ ZealandZooLIB/Secrets/ISecret.cs
@@ -8,9 +8,7 @@ namespace ZealandZooLIB.Secrets
 {
     internal interface ISecret
     {
-        // Hostname   : mssql5.unoeuro.com
-        // Brugernavn : bullerbob_dk
-        // kode       : 49xceBEtdHA36mnDwhpF
+        
         String GetSecret();
 
     }
