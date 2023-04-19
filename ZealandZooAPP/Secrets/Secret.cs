02d9b6fa95f4df7dc130852ce5e3c47aecd5c2bd
boeger01 <matias.boeger@gmail.com>
2023-04-19 10:37:24 +0200

    secret folder add skal bruges til connection string hvor kode og host navn er gemt

---
+++ ZealandZooAPP/Secrets/Secret.cs
@@ -0,0 +1,12 @@
+﻿namespace ZealandZooAPP.Secrets
+{
+    public class Secret
+    {
+
+        // Hostname: mssql5.unoeuro.com
+        // username: bullerbob_dk
+        // Pasword : 49xceBEtdHA36mnDwhpF
+
+
+    }
+}
