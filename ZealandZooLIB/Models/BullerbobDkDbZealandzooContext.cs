f8e9b82122c54788680305fc24949384260c58ec
boeger01 <matias.boeger@gmail.com>
2023-05-02 09:30:01 +0200

    serect change line

--- ZealandZooLIB/Models/BullerbobDkDbZealandzooContext.cs
+++ ZealandZooLIB/Models/BullerbobDkDbZealandzooContext.cs
@@ -29,7 +29,7 @@ public partial class BullerbobDkDbZealandzooContext : DbContext
 
     protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
 #warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see http://go.microsoft.com/fwlink/?LinkId=723263.
-        => optionsBuilder.UseSqlServer("Data Source=mssql5.unoeuro.com;User ID=bullerbob_dk;Password=49xceBEtdHA36mnDwhpF;Initial Catalog=bullerbob_dk_db_zealandzoo;Connect Timeout=30;Encrypt=False;Trust Server Certificate=False;Application Intent=ReadWrite;Multi Subnet Failover=False");
+        => optionsBuilder.UseSqlServer(Secret.GetSecret());
 
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
