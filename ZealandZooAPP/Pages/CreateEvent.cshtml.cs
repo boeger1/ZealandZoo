d5d312b58c4553d56900b2b1259dc0dccabb6080
boeger01 <matias.boeger@gmail.com>
2023-05-03 11:23:32 +0200

    create event page

---
+++ ZealandZooAPP/Pages/CreateEvent.cshtml.cs
@@ -0,0 +1,15 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace ZealandZooAPP.Pages
+{
+    public class CreateEventModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+
+
+
+}
--- ZealandZooAPP/Pages/EventPage.cshtml.cs
+++ ZealandZooAPP/Pages/EventPage.cshtml.cs
@@ -2,6 +2,7 @@ using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using ZealandZooLIB.Models;
 
+
 namespace ZealandZooAPP.Pages
 {
     public class EventPageModel : PageModel
