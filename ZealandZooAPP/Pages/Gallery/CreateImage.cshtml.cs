e6160ccd60234895ad73bd82a9a0885db72e5543
Peno-Home <peno.ing@gmail.com>
2023-05-08 14:34:20 +0200

    Tilføjet createimage razorpage

---
+++ ZealandZooAPP/Pages/Gallery/CreateImage.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace ZealandZooAPP.Pages.Gallery
+{
+    public class CreateImageModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
