a8b9ebadb96e739ac5d3a9192c4b9896482018fb
boeger01 <matias.boeger@gmail.com>
2023-05-16 15:22:18 +0200

    style event

--- ZealandZooAPP/wwwroot/js/site.js
+++ ZealandZooAPP/wwwroot/js/site.js
@@ -3,25 +3,3 @@
 
 // Write your JavaScript code.
 // Check if the current page is the about page
-function changeBackgroundImage() {
-    //if (window.location.pathname.includes("Calender")) {
-    //    // Get a reference to the body element
-    //    var body = document.getElementsByTagName('body')[0];
-
-    //    // Set the background image
-    //    body.style.backgroundImage = "url('../assets/img/beer-drink-wallpaper.jpg')";
-    //}
-
-    if (window.location.pathname.includes("EventPage")) {
-        // Get a reference to the body element
-        var body = document.getElementsByTagName("body")[0];
-
-        // Set the background image
-        body.style.backgroundImage = "url('../Images/Festival.PNG')";
-
-    }
-}
-
-
-// Call the method when the page is loaded
-window.onload = changeBackgroundImage;
