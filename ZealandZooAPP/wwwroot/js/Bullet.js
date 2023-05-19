5ac4f4968fd16863f2af0dff8a4d9534ab95e558
woodgates93 <sarah.woodgates@gmail.com>
2023-05-19 12:25:36 +0200

    modal på bullets

---
+++ ZealandZooAPP/wwwroot/js/Bullet.js
@@ -0,0 +1,9 @@
+﻿@section scripts{
+    <script>
+        $(function(){
+        if($('#searchResultTable').length > 0){
+            $('#details-modal').modal('show');
+        }
+    });
+    </script>
+}
