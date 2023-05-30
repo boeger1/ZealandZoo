998d4cf2d5be55d97dadec197f926ad319ab5b56
pete74s9 <119295287+pete74s9@users.noreply.github.com>
2023-05-30 08:24:59 +0200

    megere fejl

--- ZealandZooAPP/Pages/Index.cshtml.cs
+++ ZealandZooAPP/Pages/Index.cshtml.cs
@@ -3,31 +3,33 @@ using ZealandZooLIB.Models;
 using ZealandZooLIB.Services;
 
 namespace ZealandZooAPP.Pages
-{ /// <summary>
-/// Sarah har stået for alt der har med bulllets at gøre på denne side
-/// </summary>
-
-public class IndexModel : PageModel
 {
+    /// <summary>
+    /// Sarah har stået for alt der har med bulllets at gøre på denne side
+    /// </summary>
 
-    public BulletRepoService Bullet;
-
-    public EventRepoService Event;
-
-
-    public IndexModel(BulletRepoService bullet, EventRepoService _event)
+    public class IndexModel : PageModel
     {
-        Bullet = bullet;
-        Event = _event;
-    }
 
-    public List<BaseModel> Bullets { get; private set; }
-    public List<BaseModel> Events { get; set; }
+        public BulletRepoService Bullet;
+
+        public EventRepoService Event;
 
 
-    public void OnGet()
-    {
-        Bullets = Bullet.GetAll();
-        Events = Event.GetAll();
+        public IndexModel(BulletRepoService bullet, EventRepoService _event)
+        {
+            Bullet = bullet;
+            Event = _event;
+        }
+
+        public List<BaseModel> Bullets { get; private set; }
+        public List<BaseModel> Events { get; set; }
+
+
+        public void OnGet()
+        {
+            Bullets = Bullet.GetAll();
+            Events = Event.GetAll();
+        }
     }
 }
